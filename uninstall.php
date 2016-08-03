ec25e75e4c29cde599b67822156a9caa6e51e4a1
SGr33n <sgr33n@ircaserta.com>
2016-08-03 18:26:23 +0200

    Create uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Remove data written by the Remove Versions Plugin for WordPress after an administrative user clicks "Delete" from the plugin management page in the WordPress administrative interface (wp-admin).
+ *
+ * @since 1.0
+ */
+
+// only execute as part of an uninstall script
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
+	exit();
